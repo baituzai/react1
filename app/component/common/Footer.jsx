b6488c7ea822ec876891f3a79e55bf35ded6307e
sunday <sunday_ever@163.com>
2018-06-29 11:26:03 +0800

    Extract common components

---
+++ app/component/common/Footer.jsx
@@ -0,0 +1,9 @@
+// Footer.jsx
+
+import React from 'react';
+// import'../../public/css/common.pcss';
+const Footer = ()=>
+    <div className="buttom">
+        <div>这是底部</div>
+    </div>
+export default Footer;
--- app/component/index/Index.jsx
+++ app/component/index/Index.jsx
@@ -1,12 +1,14 @@
 // index.jsx
 import React from 'react';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
 import'../../public/css/common.pcss';
 import'../../public/css/index.pcss';
 class Index extends React.Component{
     render() {
         return (
             <div className="cont">
-                <div className="top">
+                {/* <div className="top">
                     <div>这是头部</div>
                     <div>
                         <i className="logo"/>
@@ -15,13 +17,17 @@ class Index extends React.Component{
                         <a href="index.html">headerpager</a>
                         <a href="shop.html">shop</a>
                     </div>
-                </div>
+                </div> */}
+                <Header/>
 
                 
                 <div className="index">this is headerpage</div>    
               
-                <div className="buttom"></div>
-                <div>这是底部</div>
+                {/* <div className="buttom">
+                    <div>这是底部</div>
+                </div> */}
+                <Footer/>
+                
             </div>
         );
     }
